8481bbdf677051c3f96f379c6b9d871c77c6332d
chinhan99 <braydenyong@yahoo.com.sg>
2022-07-11 13:08:51 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { writable } from "svelte/store";
+  import Modal, { bind } from "svelte-simple-modal";
+  import Popup from "./Popup.svelte";
+  import Counter from "./Counter.svelte";
+  const modal = writable(null);
+  const showModal = () => modal.set(bind(Popup, { message: "Surprise" }));
+</script>
+
+<Modal show={$modal}>
+  <button on:click={showModal}>Show modal</button>
+  <Counter />
+</Modal>
